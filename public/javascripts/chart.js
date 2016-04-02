edfcfa5ac7e758026c2418d065086ffd588964b4
Christopher Kurek <cakurek1@gmail.com>
2016-04-01 19:30:01 -0500

    Rotated labels, added bar transitions, splash screen

---
+++ public/javascripts/chart.js
@@ -0,0 +1,158 @@
+/**
+ * This class builds a bar chart for display obesity data
+ * @constructor
+ */
+function BarChart() {
+    // Chart variables
+    this.wWidth = 0;        /* Browser window width */
+    this.wHeight = 0;       /* Browser window height */
+    this.width = 0;         /* Chart width */
+    this.height = 0;        /* Chart height */
+    this.chart = null;      /* D3 Chart object */
+    this.x = null;
+    this.y = null;
+    this.margin = {
+        top: 30,
+        bottom: 150,
+        left: 50,
+        right: 20
+    };                      /* Chart margins */
+    this.data = null;       /* D3 parsed CSV data */
+    this.countryIndexSelected = 0;  /* Which country to display data for */
+    this.ageGroupsLabelsMap = null; /* Map of data sorted by age group */
+    this.ageGroupLabelIds = null;     /* Age group labels */
+    this.ageGroupLabels = null;
+    this.meansNestedByLocationAgeGroupId = null;    /* Nested data (i.e.: Location.AgeGroup.Sex) */
+}
+
+    /**
+     * This method sets up the chart size dynamically based on the
+     *  page dimensions
+     *
+     * @param countryIndex - Which country to display data for
+     */
+    BarChart.prototype.drawChart = function (countryIndex) {
+        if (countryIndex) this.countryIndexSelected = countryIndex;
+        var self = this;    /* To keep the right context in nested functions */
+
+        this.wWidth = $(".container").width();
+        this.wHeight = window.innerHeight * 0.75;
+
+        this.width = this.wWidth - this.margin.left - this.margin.right;
+        this.height = this.wHeight - this.margin.top - this.margin.bottom;
+
+        if (this.chart !== null && this.chart !== undefined) {
+            this.chart.remove()
+        }
+
+        this.chart = d3.select(".chart")
+            .attr("width", this.width + this.margin.left + this.margin.right)
+            .attr("height", this.height + this.margin.top + this.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+
+        this.chart.selectAll(".chart_heading").remove();
+
+        this.x = d3.scale.ordinal()
+            .rangeRoundBands([0,this.width],.1);
+
+        this.y = d3.scale.linear()
+            .range([this.height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(this.x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(this.y)
+            .orient("left")
+            .tickFormat(d3.format(".0%"));
+
+        this.x.domain(this.ageGroupLabelIds);
+        this.y.domain([0, d3.max(this.data, function(d) { return +d.prevalence; })]);
+
+        // -- Draw heading -- //
+        this.chart.append("text")
+            .attr("class", "chart_heading")
+            .attr("x", (this.width) / 2)
+            .attr("y", 10)
+            .text("Obesity Rate vs Age, " + this.meansNestedByLocationAgeGroupId[this.countryIndexSelected].key);
+
+        // -- Draw axes & labels -- //
+
+        // translate x-axis' vertical position
+        this.chart.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + this.height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("y", 0)
+            .attr("x", 9)
+            .attr("dy", ".35em")
+            .attr("transform", "rotate(90)")
+            .style("text-anchor", "start")
+            .attr("x", 10)
+            .attr("y", -1)
+            .text(function(d, i) { return self.ageGroupLabels[i]; });
+        this.chart
+            .append("text")
+            .attr("x", this.width / 2 - (this.margin.left / 2))
+            .attr("y", this.height + 120)
+            .style("text-anchor", "middle")
+            .text("Age Groups");
+
+        this.chart.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+        this.chart
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -40)
+            .attr("x", -(this.height / 2))
+            .style("text-anchor", "middle")
+            .text("Obesity Prevalence");
+
+        // Draw the bars representing the data
+        this.drawData();
+    };
+
+    /**
+     * This method draws the axes, labels, and bars for the chart
+     *
+     * @param countryIndex - Which country to display data for
+     */
+    BarChart.prototype.drawData = function (countryIndex) {
+        if (countryIndex) this.countryIndexSelected = countryIndex;
+        var self = this;    /* To keep the right context in nested functions */
+
+        // -- Draw bars -- //
+
+        // Bind .bar elements to data
+        var bars = this.chart.selectAll(".bar")
+            .data(this.meansNestedByLocationAgeGroupId[this.countryIndexSelected].values);
+        // Back out to the parent node (g), shrink and remove any
+        //  bars left from last draw.
+        // fill-opacity value explained here:
+        // https://github.com/mbostock/d3/wiki/Transitions#d3_interpolateNumber
+        bars.exit()
+            .transition()
+            .duration(300)
+            .attr("y", self.y(0))
+            .attr("height", self.height - self.y(0))
+            .style('fill-opacity', 1e-6)
+            .remove();
+        // Enter into the data yet to be bound and bind each to a rect
+        bars.enter().append("rect")
+            .attr("class", "bar")
+            .attr("y", self.y(0))
+            .attr("height", self.height - self.y(0));
+        // Define all .bar element transitions
+        bars
+            .transition()
+            .duration(300)
+            .ease("quad")
+            .attr("x", function(d) { return self.x(+d.key); })
+            .attr("y", function(d) { return self.y(+d.values); })
+            .attr("height", function(d) { return self.height - +self.y(+d.values); })
+            .attr("width", self.x.rangeBand());
+    };
--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -5,139 +5,56 @@ $(document).ready(function(){
 
 // Page variables
 var menuPopulated = false;
-var countryIndexSelected = 0;
-
-// Data Variables
-var data;
-var ageGroupsLabelsMap;
-var ageGroupLabels;
-var meansNestedByLocationAgeGroupId;
-
 
 // Chart variables
-var wWidth, wHeight, width, height, chart,
-    margin = {top: 20, right: 20, bottom: 50, left: 50};
+var ch = new BarChart();
 
 // Parse data and begin visualization
 var d3_dataset = d3.csv("/data/IHME_GBD_2013_OBESITY_PREVALENCE_1990_2013_Y2014M10D08.CSV", filterRows, function (_data) {
-    data = _data;
-    ageGroupsLabelsMap = d3.map(data, function(d) { return d.ageGroupId; });
-    ageGroupLabels = ageGroupsLabelsMap.values().map(function(d) { return d.ageGroupId; });
+    ch.data = _data;
 
+    $("#splash").remove();
+    ch.ageGroupsLabelsMap = d3.map(ch.data, function(d) { return d.ageGroup; });
+    ch.ageGroupLabelIds = ch.ageGroupsLabelsMap.values().map(function(d) { return d.ageGroupId; });
+    ch.ageGroupLabels = ch.ageGroupsLabelsMap.values().map(function(d) { return d.ageGroup; });
+    
     var nestGen = d3.nest();
-    meansNestedByLocationAgeGroupId = nestGen
+    ch.meansNestedByLocationAgeGroupId = nestGen
         .key(function(d) { return d.location; })
-        .key(function(d) { return d.ageGroupId })
+        .key(function(d) { return d.ageGroupId; })
         .rollup(function(d) { return d3.mean(d, function(s) { return +s.prevalence; }); })
-        .entries(data);
+        .entries(ch.data);
 
-    // console.log(meansNestedByLocationAgeGroupId);
-    // meansNestedByLocationAgeGroupId[0].values.forEach(function(d) { console.log(d);});
+    // console.log(ch.meansNestedByLocationAgeGroupId);
+    // ch.meansNestedByLocationAgeGroupId[0].values.forEach(function(d) { console.log(d);});
 
     if (!menuPopulated) {
+        $(".dropdown-button").show();
         // Populate the dropdown menu
         d3.select("ul").selectAll(".scrollable-menu")
-            .data(meansNestedByLocationAgeGroupId)
+            .data(ch.meansNestedByLocationAgeGroupId)
             .enter().append("li")
             .append("a")
-            .attr("href", function(d, i) { return "javascript:setCountry(" + i + ")" })
+            .attr("href", function(d, i) { return "javascript:setCountry('" + i + "')" })
             .text(function(d) { return d.key; })
         menuPopulated = true;
     }
 
     // Get window size and create chart of that size
-    drawChart(0);
-    $(window).resize(drawChart);
+    ch.drawChart();
+    drawLegend();
+    $(window).resize(function() { ch.drawChart()});
 });
 
 function filterRows(d) {
-    return { location: d.location_name, ageGroupId: d.age_group_id, ageGroup: d.age_group, prevalence: d.mean };
+    return { location: d.location_name, ageGroupId: d.age_group_id, ageGroup: d.age_group, sex: d.sex, prevalence: d.mean };
 }
 
-
-// -- datavis Functions -- //
-
-
-function drawChart() {
-    wWidth = $(".container").width();
-    wHeight = window.innerHeight * 0.75;
-
-    width = wWidth - margin.left - margin.right;
-    height = wHeight - margin.top - margin.bottom;
-
-    if (chart) {
-        chart.remove()
-    }
-
-    chart = d3.select(".chart")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    drawData(0);
-}
 
 function setCountry(selectedIndex) {
-    countryIndexSelected = selectedIndex;
-    drawData();
+    ch.drawData(selectedIndex);
 }
 
-function drawData() {
+function drawLegend() {
 
-    chart.selectAll("rect, .chart_heading").remove();
-
-    chart.append("text")
-        .attr("class", "chart_heading")
-        .attr("x", (width) / 2)
-        .attr("y", 10)
-        .text("Obesity Rate vs Age, " + meansNestedByLocationAgeGroupId[countryIndexSelected].key)
-
-    var barWidth = width / ageGroupsLabelsMap.keys().length;
-
-    var x = d3.scale.ordinal()
-        .rangeRoundBands([0,width],.1);
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    x.domain(ageGroupLabels);
-    y.domain([0, d3.max(data, function(d) { return +d.prevalence; })]);
-
-    // translate x-axis' vertical position
-    chart.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .append("text")
-        .attr("x", width / 2)
-        .attr("y", 40)
-        .style("text-anchor", "middle")
-        .text("Age Group");
-
-    chart.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -40)
-        .attr("x", -(height / 2))
-        .style("text-anchor", "middle")
-        .text("Obesity Prevalence");
-
-    chart.selectAll(".bar")
-        .data(meansNestedByLocationAgeGroupId[countryIndexSelected].values)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(+d.key); })
-        .attr("y", function(d) { return y(+d.values); })
-        .attr("height", function(d) { return height - y(+d.values); })
-        .attr("width", x.rangeBand());
 }
